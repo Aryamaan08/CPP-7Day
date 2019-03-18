11f101d512cb29b98860eec693ac9a824e7ee178
Aryamaan08 <44227544+Aryamaan08@users.noreply.github.com>
2019-03-18 23:17:20 +0530

    First program...

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Hey, I know at least this!
+
+using namespace std; // And this
+
+int main() { // I know this from C
+  cout << "Hello World!\n"; // << because this is the output. Otherwise cin >>
+  return 0; // I know this from C
+  /* Run:
+  Hello World!
+
+  * /
+}
+
+// Yes! It worked!
